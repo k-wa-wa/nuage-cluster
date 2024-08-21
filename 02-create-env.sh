837ffc955aaf9144f1c8e8e0b446fb9850a745bb
watanabe <sek.ohei.w0822@icloud.com>
2024-08-21 23:36:14 +0900

    change: VM(on Windows/multipass)を削除、ラズパイで置き換え

--- 02-create-env.sh
+++ 02-create-env.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 ansible_hosts_yml=`cat playbooks/hosts_template.yml`
 
-# mac-1
-vms=('node-1')
-for vm in ${vms[@]}
-do
-  host=$(multipass info $vm | grep "192.168.xxx" | tr -d " " )
-  ansible_hosts_yml=${ansible_hosts_yml//\[\["$vm"_host\]\]/$host}
-  ansible_hosts_yml=${ansible_hosts_yml//\[\["$vm"_user\]\]/ubuntu}
-done
+# raspi
+host=$(ssh nuage@pi-1.local -i .ssh/id_rsa "hostname -I")
+ansible_hosts_yml=${ansible_hosts_yml//\[\[node-1_host\]\]/$host}
+ansible_hosts_yml=${ansible_hosts_yml//\[\[node-1_user\]\]/nuage}
 
-# win-1
-vms=('control-plane-2' 'node-2' 'control-plane-1')
-for vm in ${vms[@]}
-do
-  host=$(ssh -F .ssh-host/config win-1 'vagrant ssh '${vm}' -c "hostname -I"' | sed 's/.*\(192\.168\.xxx\.[0-9]*\).*/\1/')
-  ansible_hosts_yml=${ansible_hosts_yml//\[\["$vm"_host\]\]/$host}
-  ansible_hosts_yml=${ansible_hosts_yml//\[\["$vm"_user\]\]/vagrant}
-done
+host=$(ssh nuage@pi-2.local -i .ssh/id_rsa "hostname -I")
+ansible_hosts_yml=${ansible_hosts_yml//\[\[node-2_host\]\]/$host}
+ansible_hosts_yml=${ansible_hosts_yml//\[\[node-2_user\]\]/nuage}
+
+host=$(ssh nuage@pi-3.local -i .ssh/id_rsa "hostname -I")
+ansible_hosts_yml=${ansible_hosts_yml//\[\[control-plane-1_host\]\]/$host}
+ansible_hosts_yml=${ansible_hosts_yml//\[\[control-plane-1_user\]\]/nuage}
 
 # intelmac-1
-vms=('control-plane-3' 'node-3')
+vms=('control-plane-3' 'node-3' 'control-plane-2')
 for vm in ${vms[@]}
 do
   host=$(ssh -F .ssh-host/config intelmac-1 'export PATH=/usr/local/bin:$PATH; vagrant ssh '${vm}' -c "hostname -I"' | sed 's/.*\(192\.168\.xxx\.[0-9]*\).*/\1/')
