bf57549811a49c7efb9eba9cdfc5463a6635ceff
watanabe <sek.ohei.w0822@icloud.com>
2024-08-28 22:27:08 +0900

    change: クラスタをシングルコントロールプレーン構成に変更

--- 02-create-env.sh
+++ 02-create-env.sh
@@ -2,26 +2,30 @@
 ansible_hosts_yml=`cat playbooks/hosts_template.yml`
 
 # raspi
-host=$(ssh nuage@pi-1.local -i .ssh/id_rsa "hostname -I")
+host=$(ssh -o StrictHostKeyChecking=no nuage@pi-1.local -i .ssh/id_rsa "hostname -I")
 ansible_hosts_yml=${ansible_hosts_yml//\[\[node-1_host\]\]/$host}
 ansible_hosts_yml=${ansible_hosts_yml//\[\[node-1_user\]\]/nuage}
 
-host=$(ssh nuage@pi-2.local -i .ssh/id_rsa "hostname -I")
+host=$(ssh -o StrictHostKeyChecking=no nuage@pi-2.local -i .ssh/id_rsa "hostname -I")
 ansible_hosts_yml=${ansible_hosts_yml//\[\[node-2_host\]\]/$host}
 ansible_hosts_yml=${ansible_hosts_yml//\[\[node-2_user\]\]/nuage}
 
-host=$(ssh nuage@pi-3.local -i .ssh/id_rsa "hostname -I")
-ansible_hosts_yml=${ansible_hosts_yml//\[\[control-plane-1_host\]\]/$host}
-ansible_hosts_yml=${ansible_hosts_yml//\[\[control-plane-1_user\]\]/nuage}
+host=$(ssh -o StrictHostKeyChecking=no nuage@pi-3.local -i .ssh/id_rsa "hostname -I")
+ansible_hosts_yml=${ansible_hosts_yml//\[\[node-3_host\]\]/$host}
+ansible_hosts_yml=${ansible_hosts_yml//\[\[node-3_user\]\]/nuage}
 
 # intelmac-1
-vms=('control-plane-3' 'node-3' 'control-plane-2')
+vms=('node-4')
 for vm in ${vms[@]}
 do
   host=$(ssh -F .ssh-host/config intelmac-1 'export PATH=/usr/local/bin:$PATH; vagrant ssh '${vm}' -c "hostname -I"' | sed 's/.*\(192\.168\.xxx\.[0-9]*\).*/\1/')
   ansible_hosts_yml=${ansible_hosts_yml//\[\["$vm"_host\]\]/$host}
   ansible_hosts_yml=${ansible_hosts_yml//\[\["$vm"_user\]\]/vagrant}
 done
+
+host=192.168.xxx.xxx
+ansible_hosts_yml=${ansible_hosts_yml//\[\[control-plane-1_host\]\]/$host}
+ansible_hosts_yml=${ansible_hosts_yml//\[\[control-plane-1_user\]\]/nuage}
 
 # done
 echo "$ansible_hosts_yml"
