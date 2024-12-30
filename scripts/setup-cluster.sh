934f7e8f2516360ebcc18633c1c4b36474010706
watanabe <sek.ohei.w0822@icloud.com>
2024-12-30 18:12:33 +0900

    update: 並行実行数を変更

--- scripts/setup-cluster.sh
+++ scripts/setup-cluster.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/vm/hosts.yml playbooks/vm/site-k8s.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v --forks 100 -i playbooks/vm/hosts.yml playbooks/vm/site-k8s.yml
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/k8s/hosts.yml playbooks/k8s/site.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v --forks 100 -i playbooks/k8s/hosts.yml playbooks/k8s/site.yml
 
 cp playbooks/k8s/admin.conf playbooks/k8s/admin.conf.cp
 sed 's/nuage-cluster-endpoint/192\.168\.5\.50/g' playbooks/k8s/admin.conf.cp > playbooks/k8s/admin.conf
