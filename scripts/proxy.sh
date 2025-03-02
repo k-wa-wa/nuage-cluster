913d7185ead470db08ef0bb8768a493b52c7731a
watanabe <sek.ohei.w0822@icloud.com>
2025-03-02 10:26:42 +0900

    add: proxyサーバーを追加

---
+++ scripts/proxy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/vm/hosts.yml playbooks/vm/site-proxy.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/proxy/hosts.yml playbooks/proxy/site.yml
