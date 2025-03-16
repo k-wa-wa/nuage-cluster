fda15f1f688088c7bd515ea8b02cbfc7a7ca91fa
watanabe <sek.ohei.w0822@icloud.com>
2025-03-16 14:36:55 +0900

    clean: scriptsディレクトリを整理

--- scripts/vm-service/nfs.sh
+++ scripts/vm-service/nfs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/vm/hosts.yml playbooks/vm/site-nfs.yml
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/nfs/hosts.yml playbooks/nfs/site.yml
