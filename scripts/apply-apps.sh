b77ab14994c05788748b1e74bd18e7cbc536c5e1
watanabe <sek.ohei.w0822@icloud.com>
2025-04-19 01:25:50 +0900

    add: node label, affinityを仮で付与

--- scripts/apply-apps.sh
+++ scripts/apply-apps.sh
@@ -8,6 +8,9 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
+#################### nodes ####################
+./k apply -f manifests/node-labels.yaml
+
 #################### postgres ####################
 ./k apply -f manifests/postgres/
 ./k wait --for=condition=Ready --timeout=300s pod/postgres-0
