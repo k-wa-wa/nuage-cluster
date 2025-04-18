e08c15473bb08d42aa94abe752d1c59d00e83533
watanabe <sek.ohei.w0822@icloud.com>
2025-04-19 01:16:48 +0900

    update: db作成スクリプト更新を反映

--- db/postgres/init.sh
+++ db/postgres/init.sh
@@ -4,4 +4,5 @@ apt update && apt install -y curl
 export PGUSER=$POSTGRES_USER
 psql -c "CREATE DATABASE pechka;"
 
-curl -s https://raw.githubusercontent.com/k-wa-wa/pechka/refs/heads/master/file-server/db/init.sql | psql -d pechka
+curl -s https://raw.githubusercontent.com/k-wa-wa/pechka/refs/heads/master/file-server/db/000_init.sql | psql -d pechka
+curl -s https://raw.githubusercontent.com/k-wa-wa/pechka/refs/heads/master/file-server/db/001_create_playlists_table.sql | psql -d pechka
