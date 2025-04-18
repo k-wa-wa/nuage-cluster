1d8e62f61a105617230c06f5871835bb2d278956
watanabe <sek.ohei.w0822@icloud.com>
2025-04-19 01:20:47 +0900

    add: smb backup server

---
+++ scripts/vm-service/smb.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/vm/hosts.yml playbooks/vm/site-smb.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/smb/hosts.yml playbooks/smb/site.yml
