fd128c7c3c865376feb7ab9115b3cf8ac062959a
watanabe <sek.ohei.w0822@icloud.com>
2025-08-02 22:11:51 +0900

    change: proxy,dnsのvmもterraformで作成する

---
+++ terraform/environments/standalone/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.80.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  insecure = true
+  endpoint = var.proxmox_endpoint
+  username = var.proxmox_username
+  password = var.proxmox_password
+}
+
+module "pve_vm" {
+  source = "../../modules/vm"
+  vms_config = {
+    "oc1-proxy" = {
+      vm_id     = 1160
+      node_name = "server1"
+      cores     = 2
+      memory    = 4096
+      ip_config = [
+        {
+          address = "192.168.5.160/24"
+          gateway = "192.168.5.1"
+        },
+        {
+          address = "192.168.1.70/24"
+          gateway = "192.168.1.1"
+        },
+      ]
+      ci_user   = "ubuntu"
+      disk_size = 20
+    },
+    "oc1-dns" = {
+      vm_id     = 1161
+      node_name = "server1"
+      cores     = 1
+      memory    = 2048
+      ip_config = [
+        {
+          address = "192.168.5.161/24"
+          gateway = "192.168.5.1"
+        }
+      ]
+      ci_user   = "ubuntu"
+      disk_size = 20
+    }
+  }
+}
