8fd8bc954fa59285bc4cf70961421f52750624b7
watanabe <sek.ohei.w0822@icloud.com>
2025-08-21 00:22:08 +0900

    add: ansible collectionのインストール

--- scripts/prerequire.sh
+++ scripts/prerequire.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+#################### ansible collection ####################
+ansible-galaxy collection install prometheus.prometheus --force
+ansible-galaxy collection install grafana.grafana --force
+
 #################### helm chat更新 ####################
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
