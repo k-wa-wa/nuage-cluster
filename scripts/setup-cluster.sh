28ed0b162b4639bc76d82a97bba5f5e96e3cb5d2
watanabe <sek.ohei.w0822@icloud.com>
2025-09-19 00:43:04 +0900

    refactor: cluster作成のansibleは冪等性がないため、切り出し

---
+++ scripts/setup-cluster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+    -v \
+    -i playbooks/k8s/hosts.yml \
+    playbooks/k8s/site-init-nodes.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+    -v \
+    -i playbooks/k8s/hosts.yml \
+    playbooks/k8s/site-setup-loadbalancer.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+    -v \
+    -i playbooks/k8s/hosts.yml \
+    playbooks/k8s/site-init-control-plane.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+    -v \
+    --limit "control-plane[0]" \
+    -i playbooks/k8s/hosts.yml \
+    playbooks/k8s/site-get-join-command.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+    -v \
+    --limit "control-plane[1:],worker-node" \
+    -i playbooks/k8s/hosts.yml \
+    playbooks/k8s/site-join-nodes.yml
--- scripts/setup-infra.sh
+++ scripts/setup-infra.sh
@@ -17,30 +17,3 @@ ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/dns/hosts.yml playbooks/dns/site.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -v -i playbooks/proxy/hosts.yml playbooks/proxy/site.yml
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-    -v \
-    -i playbooks/k8s/hosts.yml \
-    playbooks/k8s/site-init-nodes.yml
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-    -v \
-    -i playbooks/k8s/hosts.yml \
-    playbooks/k8s/site-setup-loadbalancer.yml
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-    -v \
-    -i playbooks/k8s/hosts.yml \
-    playbooks/k8s/site-init-control-plane.yml
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-    -v \
-    --limit "control-plane[0]" \
-    -i playbooks/k8s/hosts.yml \
-    playbooks/k8s/site-get-join-command.yml
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-    -v \
-    --limit "control-plane[1:],worker-node" \
-    -i playbooks/k8s/hosts.yml \
-    playbooks/k8s/site-join-nodes.yml
