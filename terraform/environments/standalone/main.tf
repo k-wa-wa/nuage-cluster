06439093f922613a70bd61f111567e24fb0ebb65
watanabe <sek.ohei.w0822@icloud.com>
2025-09-22 03:52:05 +0900

    refactor: リファクタ方針(VM定義を分割)

--- terraform/environments/standalone/main.tf
+++ terraform/environments/standalone/main.tf
@@ -14,29 +14,41 @@ provider "proxmox" {
   password = var.proxmox_password
 }
 
+module "oc1-proxy" {
+  source = "../../modules/ubuntu-vm"
+
+  vm_config = {
+    vm_id = 1160
+    vm_name = "oc1-proxy"
+    node_name = "server-1"
+    cores = 2
+    memory = 4096
+    network_devices = [
+      {
+        bridge = "vmbr0"
+      },
+      {
+        bridge = "vmbr1"
+      }
+    ]
+    ip_config = [
+      {
+        address = "192.168.5.160/24"
+        gateway = "192.168.5.1"
+      },
+      {
+        address = "192.168.1.70/24"
+        gateway = "192.168.1.1"
+      },
+    ]
+    ci_user = "ubuntu"
+    disk_size = 20
+  }
+}
+
 module "pve_vm" {
   source = "../../modules/vm"
   vms_config = {
-    "oc1-proxy" = {
-      vm_id     = 1160
-      node_name = "server-1"
-      cores     = 2
-      memory    = 4096
-      ip_config = [
-        {
-          address = "192.168.5.160/24"
-          gateway = "192.168.5.1"
-          bridge = "vmbr0"
-        },
-        {
-          address = "192.168.1.70/24"
-          gateway = "192.168.1.1"
-          bridge = "vmbr1"
-        },
-      ]
-      ci_user   = "ubuntu"
-      disk_size = 20
-    },
     "oc1-dns" = {
       vm_id     = 1161
       node_name = "server-1"
---
+++ terraform/modules/ubuntu-vm/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.80.0"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "proxmox_vm" {
+  vm_id     = var.vm_config.vm_id
+  name      = var.vm_config.vm_name
+  node_name = var.vm_config.node_name
+
+  cpu {
+    cores = var.vm_config.cores
+  }
+
+  memory {
+    dedicated = var.vm_config.memory
+    floating  = var.vm_config.memory
+  }
+
+  initialization {
+    dynamic "ip_config" {
+      for_each = var.vm_config.ip_config
+      content {
+        ipv4 {
+          address = ip_config.value.address
+          gateway = ip_config.value.gateway
+        }
+      }
+    }
+    user_account {
+      username = var.vm_config.ci_user
+      keys     = [trimspace(data.local_file.id_rsa_pub.content)]
+    }
+  }
+
+  disk {
+    datastore_id = "local-zfs"
+    file_id      = "local:iso/noble-server-cloudimg-amd64.img"
+    interface    = "scsi0"
+    size         = var.vm_config.disk_size
+  }
+
+  dynamic "network_device" {
+    for_each = var.vm_config.network_devices
+    content {
+      bridge = network_device.value.bridge
+    }
+  }
+
+  dynamic "usb" {
+    for_each = coalesce(var.vm_config.usb, [])
+    content {
+      host = usb.value.host
+      usb3 = true
+    }
+  }
+
+  protection = var.vm_config.protection
+}
+
+data "local_file" "id_rsa_pub" {
+  filename = "../../../.ssh/id_rsa.pub"
+}
