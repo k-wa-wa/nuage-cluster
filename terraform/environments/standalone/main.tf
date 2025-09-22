13b72499b1ba79cee34c4094a96fc588ee96035f
watanabe <sek.ohei.w0822@icloud.com>
2025-09-22 11:40:48 +0900

    refactor: VM定義を分割

--- terraform/environments/standalone/main.tf
+++ terraform/environments/standalone/main.tf
@@ -16,7 +16,6 @@ provider "proxmox" {
 
 module "oc1-proxy" {
   source = "../../modules/ubuntu-vm"
-
   vm_config = {
     vm_id = 1160
     vm_name = "oc1-proxy"
@@ -46,24 +45,34 @@ module "oc1-proxy" {
   }
 }
 
+module "oc1-dns" {
+  source = "../../modules/ubuntu-vm"
+  vm_config = {
+    vm_id = 1161
+    vm_name = "oc1-dns"
+    node_name = "server-1"
+    cores = 1
+    memory = 2048
+    network_devices = [
+      {
+        bridge = "vmbr0"
+      }
+    ]
+    ip_config = [
+      {
+        address = "192.168.5.161/24"
+        gateway = "192.168.5.1"
+      }
+    ]
+    ci_user = "ubuntu"
+    disk_size = 20
+  }
+}
+
+// TODO: ubuntu-vm モジュールを使用した書き方に変更する（VMを削除できないため保留）
 module "pve_vm" {
   source = "../../modules/vm"
   vms_config = {
-    "oc1-dns" = {
-      vm_id     = 1161
-      node_name = "server-1"
-      cores     = 1
-      memory    = 2048
-      ip_config = [
-        {
-          address = "192.168.5.161/24"
-          gateway = "192.168.5.1"
-          bridge = "vmbr0"
-        }
-      ]
-      ci_user   = "ubuntu"
-      disk_size = 20
-    },
     "oc1-nfs" = {
       vm_id     = 1151
       node_name = "server-1"
@@ -80,24 +89,35 @@ module "pve_vm" {
       disk_size = 1024
       protection = true
     },
-    "oc1-bastion" = {
-      vm_id = 1170
-      node_name = "server-1"
-      cores = 2
-      memory = 4096
-      ip_config = [
-        {
-          address = "192.168.5.170/24"
+  }
+}
+
+module "oc1-bastion" {
+  source = "../../modules/ubuntu-vm"
+  vm_config = {
+    vm_id = 1162
+    vm_name = "oc1-bastion"
+    node_name = "server-1"
+    cores = 2
+    memory = 4096
+    network_devices = [
+      {
+        bridge = "vmbr0"
+      },
+      {
+        bridge = "vmbr2"
+      }
+    ]
+    ip_config = [
+      {
+          address = "192.168.5.162/24"
           gateway = "192.168.5.1"
-          bridge = "vmbr0"
         },
         {
           address = "192.168.20.1/24"
-          bridge = "vmbr2"
         }
-      ],
-      ci_user = "ubuntu"
-      disk_size = 20
-    }
+    ]
+    ci_user = "ubuntu"
+    disk_size = 20
   }
 }
