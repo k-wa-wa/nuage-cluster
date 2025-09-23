e77cf95aa28c43410b8c5795cf36eed9205949dd
watanabe <sek.ohei.w0822@icloud.com>
2025-09-23 16:21:47 +0900

    update: hostsファイル生成→ノード/クラスター作成とする

--- scripts/setup-pve-on-pve.sh
+++ scripts/setup-pve-on-pve.sh
@@ -3,6 +3,10 @@ set -eu
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
     -v \
+    playbooks/pve-on-pve/site-generate-hosts.yml
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+    -v \
     -i playbooks/pve-on-pve/hosts.yml playbooks/pve-on-pve/site-setup-nodes.yml \
     --ssh-extra-args='-o ConnectionAttempts=30 -o ConnectTimeout=10'
 
