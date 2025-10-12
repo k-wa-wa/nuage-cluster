25950f81cd1321bc285da3e6de7c649118c2f9c3
watanabe <sek.ohei.w0822@icloud.com>
2025-10-12 17:03:49 +0900

    vibe: gatewayをgrpcからgraphqlに置き換え

--- monitoring/gateway/test.sh
+++
@@ -1,3 +0,0 @@
-grpcurl -plaintext --proto proto/report_service.proto -d '{}' localhost:5051 gateway.ReportService/ListReports
-
-grpcurl -plaintext --proto proto/report_service.proto -d '{"report_id": "0fbc3ad7-e940-4f58-b9db-ce8908ec78cb"}' localhost:5051 gateway.ReportService/GetReport
