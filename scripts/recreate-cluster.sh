87a2f05eae525359d10611788e32526b6a649ed1
watanabe <sek.ohei.w0822@icloud.com>
2025-10-18 23:12:37 +0900

    fix: ディレクトリ名修正もれ

--- scripts/recreate-cluster.sh
+++ scripts/recreate-cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-ANSIBLE_CONFIG=playbooks-new/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/k8s.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/k8s.yml
