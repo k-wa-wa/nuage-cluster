cde2e45e3cc5be790d8382432ce04ac264d9bab6
watanabe <sek.ohei.w0822@icloud.com>
2025-10-18 23:25:49 +0900

    update: http用のloadbalance追加

--- monitoring/ui/constants/config.ts
+++ monitoring/ui/constants/config.ts
@@ -1,1 +1,1 @@
-export const graphqlEndpoint = process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT || "";
+export const graphqlEndpoint = process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT || "http://monitoring.dev.nuage:81/graphql"
