a60e7e58a72975803d86169d496933f106f5ecfe
watanabe <sek.ohei.w0822@icloud.com>
2025-10-18 20:41:11 +0900

    fix: ディレクトリ名修正もれ

--- scripts/apply-infra.sh
+++ scripts/apply-infra.sh
@@ -3,6 +3,6 @@ set -eu
 
 tofu -chdir=terraform/environments/dev apply --auto-approve
 
-ANSIBLE_CONFIG=playbooks-new/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/data-store.yml
-ANSIBLE_CONFIG=playbooks-new/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/dns.yml
-ANSIBLE_CONFIG=playbooks-new/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/proxy.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/data-store.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/dns.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/proxy.yml
