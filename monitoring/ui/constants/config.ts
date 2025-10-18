324224b283748b1c0d4ebcdc8e3da9b6c62aace4
watanabe <sek.ohei.w0822@icloud.com>
2025-10-19 04:34:38 +0900

    update: スキーマ更新、mock追加

--- monitoring/ui/constants/config.ts
+++ monitoring/ui/constants/config.ts
@@ -1,1 +1,4 @@
-export const graphqlEndpoint = process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT || "http://monitoring.dev.nuage:81/graphql"
+export const graphqlEndpoint =
+  process.env.EXPO_PUBLIC_USE_MOCK_GRAPHQL === "true"
+    ? "http://localhost:4001/graphql"
+    : process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT || "http://monitoring.dev.nuage:81/graphql"
