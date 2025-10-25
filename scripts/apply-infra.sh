779728e46dc9c532c0f3fe59cbb24ce7e4e5793f
watanabe <sek.ohei.w0822@icloud.com>
2025-10-25 14:19:41 +0900

    update: 不要箇所削除、vm作成直後にansible実行してもエラーにならないようにする

--- scripts/apply-infra.sh
+++ scripts/apply-infra.sh
@@ -3,6 +3,5 @@ set -eu
 
 tofu -chdir=terraform/environments/dev apply --auto-approve
 
-ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/data-store.yml
 ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/dns.yml
 ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/proxy.yml
