9460a117b19bd06fa52357854e177ae704760157
watanabe <sek.ohei.w0822@icloud.com>
2026-01-06 21:02:15 +0900

    update: postgresのプレイブック実行

--- scripts/apply-datastore.sh
+++ scripts/apply-datastore.sh
@@ -3,4 +3,5 @@ set -eu
 
 tofu -chdir=terraform/environments/dev-persistent apply --auto-approve
 
-ANSIBLE_CONFIG=playbooks-new/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/data-store.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/data-store.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/postgres.yml
