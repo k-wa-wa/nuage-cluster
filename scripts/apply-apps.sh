ff5629da231b92bd03b4973f6b0c98bd3a67cd4c
watanabe <sek.ohei.w0822@icloud.com>
2026-01-06 23:59:17 +0900

    change: ディレクトリ変更

--- scripts/apply-apps.sh
+++ scripts/apply-apps.sh
@@ -13,7 +13,10 @@ if ! ./k get ns istio-system &> /dev/null; then
 fi
 ./k apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.0/cert-manager.yaml
 ./k apply -f istio-1.26.2/samples/addons
-./k apply -f ./manifests/base/ingress/
+
+#################### infra ####################
+./k apply -f manifests/infra/pvs/ # TODO: argocdでの管理を検討
+./k apply -f manifests/infra/ingress/
 
 #################### argocd ####################
 ./k apply -f manifests/argocd/apps/
@@ -21,8 +24,6 @@ fi
 #################### apps: dashboard ####################
 ./k apply -f manifests/dashboard-v2
 
-# apps
-./k apply -f manifests/infra/pvs/ # TODO: argocdでの管理を検討
-
+#################### apps: pechka ####################
 # ./k scale -n pechka deployment file-server-api --replicas=2
 # ./k scale -n pechka deployment file-server-ui --replicas=2
