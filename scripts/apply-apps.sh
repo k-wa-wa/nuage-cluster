90eb52cf5638bfa9501cd1ff2a21e05a2e7298d1
watanabe <sek.ohei.w0822@icloud.com>
2026-01-07 02:03:42 +0900

    docs: todoメモ

--- scripts/apply-apps.sh
+++ scripts/apply-apps.sh
@@ -7,12 +7,9 @@ export KUBECONFIG=playbooks/admin.conf
 ./k apply -k manifests/bootstrap/
 ./k apply -f manifests/apps/pg-cluster/base/endpoint.yaml
 
-#################### istio ####################
-# if ! ./k get ns istio-system &> /dev/null; then
-#   ./istio-1.26.2/bin/istioctl install -y -f istio-1.26.2/istio-custom.yaml
-# fi
-# ./k apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.0/cert-manager.yaml
-# ./k apply -f istio-1.26.2/samples/addons
+# TODO:
+# - istio-ingressgateway の作成タイミングによって、ErrImgPullBackOff になる。rollout で解消するが、要調査
+# - applicationset が作成エラーになることがある。このスクリプトを何度か実行すると解消するが依存関係の整理が必要そう
 
 #################### infra #################### TODO: argocdでの管理を検討
 ./k apply -f manifests/infra/pvs/
