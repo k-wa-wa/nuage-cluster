9ebb8706c871cb6a38de3dcc6c45c6caa92895e0
watanabe <sek.ohei.w0822@icloud.com>
2026-01-13 01:45:16 +0900

    add: dev用のVM定義を追加

--- scripts/apply-infra.sh
+++ scripts/apply-infra.sh
@@ -5,4 +5,4 @@ tofu -chdir=terraform/environments/dev apply --auto-approve
 
 ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/dns.yml
 ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/proxy.yml
-ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/omada-controller.yml
+ANSIBLE_CONFIG=playbooks/ansible.cfg ansible-playbook -i playbooks/inventory/dev/hosts.yml playbooks/devops.yml
--- terraform/environments/dev/standalone.tf
+++ terraform/environments/dev/standalone.tf
@@ -54,3 +54,28 @@ module "oc1-dns" {
   }
   depends_on = [ proxmox_virtual_environment_download_file.ubuntu_cloud_image ]
 }
+
+module "oc1-devops" {
+  source = "../../modules/ubuntu-vm"
+  vm_config = {
+    vm_id = 1162
+    vm_name = "oc1-devops"
+    node_name = "server-1"
+    cores = 4
+    memory = 16384
+    network_devices = [
+      {
+        bridge = "vmbr0"
+      }
+    ]
+    ip_config = [
+      {
+        address = "192.168.5.162/24"
+        gateway = "192.168.5.1"
+      }
+    ]
+    ci_user = "ubuntu"
+    disk_size = 200
+  }
+  depends_on = [ proxmox_virtual_environment_download_file.ubuntu_cloud_image ]
+}
