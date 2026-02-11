664ec03ed5cc94020a0cdc5d3f4e523df7064803
watanabe <sek.ohei.w0822@icloud.com>
2026-02-11 22:36:59 +0900

    add: lxcのイメージ

--- terraform/pve/iso.tf
+++ terraform/pve/iso.tf
@@ -6,3 +6,11 @@ resource "proxmox_virtual_environment_download_file" "talos_image" {
   file_name    = "talos-nocloud-amd64.iso"
   url          = "https://factory.talos.dev/image/376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba/v1.12.2/nocloud-amd64.iso"
 }
+
+resource "proxmox_virtual_environment_download_file" "lxc_ubuntu_2504" {
+  for_each     = toset(["nuc-1", "nuc-2", "server-1"])
+  content_type = "vztmpl"
+  datastore_id = "local"
+  node_name    = each.key
+  url          = "https://mirrors.servercentral.com/ubuntu-cloud-images/releases/25.04/release/ubuntu-25.04-server-cloudimg-amd64-root.tar.xz"
+}
