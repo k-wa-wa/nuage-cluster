84b03aa70d51ab91b28c88bd1d88d8b5bd763144
watanabe <sek.ohei.w0822@icloud.com>
2026-02-15 15:47:36 +0900

    refactor: k8s作成をモジュール化

---
+++ terraform/vpc/modules/k8s-cluster/variables.tf
@@ -0,0 +1,24 @@
+variable "cluster_config" {
+  type = object({
+    cluster = object({
+      name        = string
+      gateway     = string
+      endpoint    = string // vip
+      node_subnet = string
+    })
+
+    nodes = map(object({
+      type                  = string // "controlplane" or "worker_node"
+      vm_id                 = number
+      vm_name               = string
+      node_name             = string
+      cores                 = number
+      memory                = number
+      bridge                = string
+      ip_address            = string
+      cidr                  = number
+      management_ip_address = string // 255.255.255.0
+      disk_size             = number // GB
+    }))
+  })
+}
