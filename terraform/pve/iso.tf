32d98d7bd8eb9cf56c7999e4586204f181a1d4a0
watanabe <sek.ohei.w0822@icloud.com>
2026-03-01 03:45:08 +0900

    add: ubuntu24のイメージ

--- terraform/pve/iso.tf
+++ terraform/pve/iso.tf
@@ -39,3 +39,11 @@ resource "proxmox_virtual_environment_download_file" "lxc_ubuntu_2504" {
   node_name    = each.key
   url          = "https://mirrors.servercentral.com/ubuntu-cloud-images/releases/25.04/release/ubuntu-25.04-server-cloudimg-amd64-root.tar.xz"
 }
+
+resource "proxmox_virtual_environment_download_file" "lxc_ubuntu_2404" {
+  for_each     = toset(["nuc-1", "nuc-2", "server-1"])
+  content_type = "vztmpl"
+  datastore_id = "local"
+  node_name    = each.key
+  url          = "https://mirrors.servercentral.com/ubuntu-cloud-images/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64-root.tar.xz"
+}
