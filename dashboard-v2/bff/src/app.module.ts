4c3af7dc7647709adfd7bf0bcf5b87aec40f35ad
watanabe <sek.ohei.w0822@icloud.com>
2026-03-02 02:39:03 +0900

    delete: 不要リソースの削除(dashboard)

--- dashboard-v2/bff/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppsModule } from './apps/apps.module';
-
-@Module({
-  imports: [AppsModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
