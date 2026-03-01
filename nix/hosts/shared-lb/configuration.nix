eef1a6547dd802c25580dda7d1f532baee9e337d
watanabe <sek.ohei.w0822@icloud.com>
2026-03-01 23:51:57 +0900

    update: lbのvip設定、dnsの戻し(期待通り動作させられなかったため一時対応)

--- nix/hosts/shared-lb/configuration.nix
+++ nix/hosts/shared-lb/configuration.nix
@@ -6,6 +6,7 @@
     ../common/configuration.nix
     ./dns.nix
     ./haproxy.nix
+    ./keepalived.nix
   ];
 
   networking = {
--- nix/hosts/shared-lb/dns.nix
+++ nix/hosts/shared-lb/dns.nix
@@ -7,38 +7,14 @@
     config = ''
       . {
         bind 0.0.0.0
-        acl {
-            allow net 10.20.1.0/24
-            block
-        }
-
-        template ANY ANY nuage.cluster.wpc {
-          answer "{{ .Name }} 60 IN A 10.20.1.20"
-        }
-
-        forward . 8.8.8.8
         log
-      }
-
-      . {
-        bind 0.0.0.0
-        acl {
-            allow net 192.168.5.0/24
-            block
-        }
 
         template ANY ANY nuage.cluster.wpc {
           answer "{{ .Name }} 60 IN A 192.168.5.200"
+          fallthrough
         }
 
         forward . 8.8.8.8
-        log
-      }
-
-      . {
-        bind 0.0.0.0
-        forward . 8.8.8.8
-        log
       }
     '';
   };
