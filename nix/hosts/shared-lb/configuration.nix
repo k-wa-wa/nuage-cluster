67f7557f86581683a28cb4e28b26dbcd44f27bdd
watanabe <sek.ohei.w0822@icloud.com>
2026-03-01 21:57:26 +0900

    update: lbをprivate用に設定

--- nix/hosts/shared-lb/configuration.nix
+++ nix/hosts/shared-lb/configuration.nix
@@ -11,20 +11,6 @@
   networking = {
     hostName = "shared-lb";
     useDHCP = false;
-    interfaces = {
-      eth0.ipv4.addresses = [{
-        address = "10.10.1.2";
-        prefixLength = 24;
-      }];
-      eth1.ipv4.addresses = [{
-        address = "192.168.5.190";
-        prefixLength = 24;
-      }];
-      eth2.ipv4.addresses = [{
-        address = "10.0.1.2";
-        prefixLength = 24;
-      }];
-    };
     nameservers = [ "8.8.8.8" ];
   };
 
--- nix/hosts/shared-lb/dns.nix
+++ nix/hosts/shared-lb/dns.nix
@@ -7,16 +7,38 @@
     config = ''
       . {
         bind 0.0.0.0
-        
-        # ワイルドカード設定
-        template ANY ANY cluster.wpc {
-          answer "{{ .Name }} 60 IN A 10.0.1.2"
-          fallthrough
+        acl {
+            allow net 10.20.1.0/24
+            block
+        }
+
+        template ANY ANY nuage.cluster.wpc {
+          answer "{{ .Name }} 60 IN A 10.20.1.20"
         }
 
         forward . 8.8.8.8
         log
-        errors
+      }
+
+      . {
+        bind 0.0.0.0
+        acl {
+            allow net 192.168.5.0/24
+            block
+        }
+
+        template ANY ANY nuage.cluster.wpc {
+          answer "{{ .Name }} 60 IN A 192.168.5.200"
+        }
+
+        forward . 8.8.8.8
+        log
+      }
+
+      . {
+        bind 0.0.0.0
+        forward . 8.8.8.8
+        log
       }
     '';
   };
