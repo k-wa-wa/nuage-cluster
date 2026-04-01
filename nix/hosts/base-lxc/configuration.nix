c50afe5d5f05591b92c7fcd2018332befc4c7c6c
watanabe <sek.ohei.w0822@icloud.com>
2026-04-01 23:27:48 +0900

    検証: cloud-init3

--- nix/hosts/base-lxc/configuration.nix
+++ nix/hosts/base-lxc/configuration.nix
@@ -5,6 +5,10 @@
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
     ../../modules/common.nix
   ];
+  environment.systemPackages = with pkgs; [
+    cloud-init
+    ripgrep # 検証
+  ];
 
   services.cloud-init.enable = true;
   services.cloud-init.network.enable = false;
