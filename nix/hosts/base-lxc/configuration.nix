ab7fff1d970782d4318b2d35a78c5b3653f44fa6
watanabe <sek.ohei.w0822@icloud.com>
2026-04-02 01:39:39 +0900

    change: nixosでの対応はやめて、lxc側で対策する

--- nix/hosts/base-lxc/configuration.nix
+++ nix/hosts/base-lxc/configuration.nix
@@ -5,12 +5,7 @@
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
     ../../modules/common.nix
   ];
-  environment.systemPackages = with pkgs; [
-    cloud-init
-    ripgrep # 検証
-  ];
 
   services.cloud-init.enable = true;
   services.cloud-init.network.enable = true;
-  systemd.network.networks."10-cloud-init-eth0" = lib.mkForce {};
 }
