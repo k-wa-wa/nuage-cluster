bb4467dc7030d9f74b650fca7c9318ddcee5d84c
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-07-09 02:28:12 +0900

    fix: ipv6無効か

--- nix/hosts/minio-cluster/configuration.nix
+++ nix/hosts/minio-cluster/configuration.nix
@@ -11,9 +11,11 @@
     nameservers = [ "8.8.8.8" ];
   };
 
-  # 非ローカルIPアドレスのバインドを許可する（VIP運用のため）
+  # 非ローカルIPアドレスのバインドを許可する（VIP運用のため）、およびIPv6を無効化する
   boot.kernel.sysctl = {
     "net.ipv4.ip_nonlocal_bind" = 1;
+    "net.ipv6.conf.all.disable_ipv6" = 1;
+    "net.ipv6.conf.default.disable_ipv6" = 1;
   };
 
   # 各ノードのメトリクス収集用ノードエクスポーターを有効化する
