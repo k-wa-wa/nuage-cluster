1288648d45e64995944c0d28d7d682eef272818c
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-07-20 10:47:28 +0900

    delete: おそらく不要設定のため削除して検証

--- scripts/apply-apps.sh
+++ scripts/apply-apps.sh
@@ -10,7 +10,9 @@ kubectl create secret generic sops-age-key \
   --from-file=keys.txt=$HOME/.config/sops/age/argocd_key.txt \
   --dry-run=client -o yaml | kubectl apply -f -
 
+# CRDの確立を待つ
 kubectl wait --for=condition=Established crd/appprojects.argoproj.io --timeout=60s
+kubectl wait --for=condition=Established crd/applicationsets.argoproj.io --timeout=60s
 kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
 
 kubectl apply -f manifests/apps/
