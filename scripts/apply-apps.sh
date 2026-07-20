7b75a267683a5d01af82574ace335e2208a5de2c
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-07-20 12:59:03 +0900

    add: github patを使用してレートリミットを回避する

--- scripts/apply-apps.sh
+++ scripts/apply-apps.sh
@@ -9,6 +9,11 @@ kubectl create secret generic sops-age-key \
   --namespace argocd \
   --from-file=keys.txt=$HOME/.config/sops/age/argocd_key.txt \
   --dry-run=client -o yaml | kubectl apply -f -
+GITHUB_TOKEN=$(SOPS_AGE_KEY_FILE=$HOME/.config/sops/age/argocd_key.txt sops -d scripts/secrets.yaml | yq -r ".github_access_token")
+kubectl create secret generic pechka-preview-github-token \
+  --namespace argocd \
+  --from-literal=token="$GITHUB_TOKEN" \
+  --dry-run=client -o yaml | kubectl apply -f -
 
 # CRDの確立を待つ
 kubectl wait --for=condition=Established crd/appprojects.argoproj.io --timeout=60s
